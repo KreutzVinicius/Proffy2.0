4bb167031a9567995e3ff73c31c3cf146f9f0bb3
KreutzVinicius <viniciuskr2010@gmail.com>
2024-06-04 16:26:47 -0300

    feat: give classes

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,20 @@
+import "./styles.css"
+
 const Header = () => {
 
-    return <div> Header</div>
+    return <div className="page-header">
+        <div className="top-bar-container">
+            <a href="/">
+            <img src="images/icons/back.svg" alt="Voltar"/>
+            </a>
+            <img src="images/logo.svg" alt=""/>
+        </div>
+        <div className="header-content">
+            <strong>Que incrível que você quer dar aulas.</strong>
+            <p>O primeiro passo, é preencher esse formulário de inscrição</p>
+            
+        </div>
+    </div>
 
 }
 export default Header;
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,14 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
+import GiveClasses from "./pages/GiveClasses/GiveClasses";
 
 const AppRoutes = () => {
 return (
   <BrowserRouter>
     <Routes>
         <Route Component={Home} path="/" />
+        <Route Component={Home} path="/study" />
+        <Route Component={GiveClasses} path="/give-classes" />
       </Routes>
   </BrowserRouter>
 )}
