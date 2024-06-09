6e3c04b09e871fc52cd197247c976150984cc604
KreutzVinicius <viniciuskr2010@gmail.com>
2024-06-09 13:41:24 -0300

    feat: adding fetch proffy

--- src/context/proffyContext.tsx
+++ src/context/proffyContext.tsx
@@ -1,16 +1,37 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { Proffy } from '../types';
 
-interface ProffyContextData {}
+interface ProffyContextData {
+    proffys: Proffy[];
+}
 
-export const ProffyContext = createContext<ProffyContextData | undefined>(undefined);
+const proffyInitialValues: ProffyContextData = {
+    proffys: [],
+};
+
+export const ProffyContext = createContext<ProffyContextData>(proffyInitialValues);
 
 export const ProffyProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  
+    const [proffys, setProffys] = useState<Proffy[]>([]);
 
+
+    useEffect(() => {
+        if(proffys.length === 0) {
+            getProffys()
+        }
+    }, [proffys]);
+
+
+    const getProffys = async () => {
+        const response = await fetch('http://localhost:3333/proffys');
+        const data = await response.json();
+        setProffys(data);
+    }
 
     return (
         <ProffyContext.Provider
             value={{
+                proffys,
             }}
         >
             {children}
